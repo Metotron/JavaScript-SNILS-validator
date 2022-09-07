7b620d7d914af6f95ddb3dec54e06683d45f17c5
Metotron <metotron1@yahoo.com>
2022-09-07 11:01:49 +0300

    Небольшой рефакторинг

--- main.js
+++ main.js
@@ -2,14 +2,21 @@ function validateSNILS(number) {
 	number = number.toString().replace(/\D/g, '')
 
 	// Цифр всегда 11
-	if (number.length != 11) return false
+	if (number.length != 11)
+		return false
+
 	// Трёх одинаковых цифр подряд быть не может
-	if (/(\d)\1\1/.test(number.toString())) return false
+	if (/(\d)\1\1/.test(number.toString()))
+		return false
 
 	const controlCode = Number(number.substr(-2))
-	let calcCode = number.substr(0, 9).split('').map((N, idx) => Number(N) * (9 - idx)).reduce((Sum, N) => Sum + N)
-	if (calcCode == 100) calcCode = 0
-	if (calcCode % 101 != controlCode) return false
+	let hashCode = number.substr(0, 9)
+		.split('')
+		.map((N, idx) => Number(N) * (9 - idx))
+		.reduce((sum, N) => sum + N)
 
-	return true
+	if (hashCode == 100)
+		hashCode = 0
+
+	return controlCode == hashCode % 101
 }
